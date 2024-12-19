dc51f3244ab49b6ff8fccf17756c3f8be60160c0
7 Partidas Digital <7partidasdigital@gmail.com>
2024-12-19 13:56:07 +0100

    Revisión

--- R/DA_scrap.R
+++ R/DA_scrap.R
@@ -3,20 +3,20 @@
 
 # Download all files that make up the _Diccionario de Autoridades_.
 # They have divide into 69422 files 
-# The trick is to analize the URL that the on line app gives for
+# The trick is to analyze the URL that the on line app gives for
 # every entry. It is a very long string. And somewhere within that string,
 # there is a DocId= followed by a five digit number.
 # The trickiest part was to guess the upper limit. I can't remember how I
 # guess it, but I reached the last number was 69422. This number was the only
 # part that has to change to collect (harvest) all files.
-# This instuction builds the series of numbersas between 0 and 69422
+# This instruction builds the series of numbers between 0 and 69422
 
 file_index <- 1:69422
 
 # But computers are computers. They are very good handling numbers as numbers, but not
 # when they see them as character. Computers are so silly that after 1 does not came 2
 # but 10. So we have to pad the number with leading zeroes in such a way that 2 goes
-# after 1. So, as or las number is five digits long, all numbers must have four, three,
+# after 1. So, as or last number is five digits long, all numbers must have four, three,
 # two and one leading zeroes. Something that can be done easily with this command:
 
 file_index <- stringr::str_pad(1:69422, 5, pad = "0")
